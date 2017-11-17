5b385a9382e21f2a76d00e188f1d527243b04abd
Chris Lin <chris522229197@gmail.com>
2017-11-17 02:12:24 -0800

    Split data into train, train dev, dev, and test

--- split_data.py
+++ split_data.py
@@ -14,7 +14,7 @@ def batch_save(dfs):
     return
 
 def split_log(seed, dfs):
-    log_file = open('data/split_log.txt', 'w')
+    log_file = open('data/split_log2.txt', 'w')
     log_file.write('seed: ' + str(seed) + '\n')
     for name, df in dfs.items(): 
         log_file.write('=' * 15 + '\n')
@@ -27,7 +27,8 @@ def split_log(seed, dfs):
 # Global constant variables
 RAW_DATA_PATH = 'data/en_train.csv'
 TRAIN_SIZE = 500000
-DEV_SIZE = 150000
+TRAIN_DEV_SIZE = 75000
+DEV_SIZE = 75000
 SEED = 123
 
 # Set the seed for random generation
@@ -37,31 +38,21 @@ rd.seed(SEED)
 raw_data = pd.read_csv(RAW_DATA_PATH)
 num_sentences = len(raw_data['sentence_id'].unique())
 
-# Split the data based on the raw data ordering
-ordered_train_ids = list(range(TRAIN_SIZE))
-ordered_dev_ids = list(range(TRAIN_SIZE, TRAIN_SIZE + DEV_SIZE))
-ordered_test_ids = list(range(TRAIN_SIZE + DEV_SIZE, num_sentences))
-
-ordered_train = subset_sentence(raw_data, ordered_train_ids)
-ordered_dev = subset_sentence(raw_data, ordered_dev_ids)
-ordered_test = subset_sentence(raw_data, ordered_test_ids)
-
 # Split the data based on shuffled ordering
 shuffled_ids = rd.sample(list(range(num_sentences)), num_sentences)
 
-shuffled_train_ids = shuffled_ids[:TRAIN_SIZE]
-shuffled_dev_ids = shuffled_ids[TRAIN_SIZE:(TRAIN_SIZE + DEV_SIZE)]
-shuffled_test_ids = shuffled_ids[(TRAIN_SIZE + DEV_SIZE):num_sentences]
+train_ids = shuffled_ids[:TRAIN_SIZE]
+train_dev_ids = shuffled_ids[TRAIN_SIZE:(TRAIN_SIZE + TRAIN_DEV_SIZE)]
+dev_ids = shuffled_ids[(TRAIN_SIZE + TRAIN_DEV_SIZE):(TRAIN_SIZE + TRAIN_DEV_SIZE + DEV_SIZE)]
+test_ids = shuffled_ids[(TRAIN_SIZE + TRAIN_DEV_SIZE + DEV_SIZE):num_sentences]
 
-shuffled_train = subset_sentence(raw_data, shuffled_train_ids)
-shuffled_dev = subset_sentence(raw_data, shuffled_dev_ids)
-shuffled_test = subset_sentence(raw_data, shuffled_test_ids)
+train = subset_sentence(raw_data, train_ids)
+train_dev = subset_sentence(raw_data, train_dev_ids)
+dev = subset_sentence(raw_data, dev_ids)
+test = subset_sentence(raw_data, test_ids)
 
 # Save the split data as csv files
-split_data = dict(zip(['ordered_train', 'ordered_dev', 'ordered_test', 
-                       'shuffled_train', 'shuffled_dev', 'shuffled_test'], 
-                      [ordered_train, ordered_dev, ordered_test, 
-                       shuffled_train, shuffled_dev, shuffled_test]))
+split_data = dict(zip(['train', 'train_dev', 'dev', 'test'], [train, train_dev, dev, test]))
 batch_save(split_data)
 split_log(SEED, split_data)
 
