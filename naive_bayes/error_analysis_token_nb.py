c795774bc3365c262379222cdb5a5d7bcaef2960
Chris Lin <chris522229197@gmail.com>
2017-12-01 15:30:49 -0800

    Reorg files

--- naive_bayes/error_analysis_token_nb.py
+++ naive_bayes/error_analysis_token_nb.py
@@ -19,8 +19,11 @@ print('Ratio of predicted transformation: ' + str(predict_transform_ratio))
 class_counts = pd.value_counts(wrong['class'].values, sort=True)
 class_counts.rename('wrong', inplace=True)
 
-plt.figure(figsize = (8,8))
+plt.figure(figsize = (12, 12))
 class_counts.plot(kind='bar')
+plt.title('Figure 1. Disribution of token class in incorrect predictions.')
+plt.xlabel('Token class')
+plt.ylabel('Frequency')
 plt.savefig('naive_bayes/conut_per_class.png')
 
 # Normalize the counts by the total counts in the whole train dev set
@@ -29,6 +32,9 @@ total_counts.rename('train_dev', inplace=True)
 merged = pd.concat([class_counts, total_counts], axis=1, join='inner')
 merged['normalized'] = merged['wrong'] / merged['train_dev']
 
-plt.figure(figsize = (8,8))
+plt.figure(figsize = (12, 12))
 merged.sort_values('normalized', ascending=False)['normalized'].plot(kind='bar')
+plt.title('Figure 2. Normalized distribution of token class in incorrect predictions.')
+plt.xlabel('Token class')
+plt.ylabel('Normalized frequency')
 plt.savefig('naive_bayes/normalized_conut_per_class.png')
