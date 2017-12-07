95257263440007313f8cb8478d217fb8718e1371
Chris Lin <chris522229197@gmail.com>
2017-12-07 15:33:57 -0800

    Step1 multinomial naive bayes

--- step1_char_nb/train_char_nb.py
+++ step1_char_nb/train_char_nb.py
@@ -3,7 +3,6 @@
 import pickle
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
 from sklearn.naive_bayes import MultinomialNB
 import time
