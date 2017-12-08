6f3af746fcaa021439ce4041a401c1d223bc2abe
Chris Lin <chris522229197@gmail.com>
2017-12-07 19:53:37 -0800

    Test accuracy

---
+++ final_analysis/test_predict.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pickle
+import prediction_pipeline as pp
+import normalization_helpers as nh
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import MultinomialNB
+
+# Load the token-level Naive Bayes models
+with open('naive_bayes/token_nb_model.p', 'rb') as f:
+    token_nb_model = pickle.load(f)
+
+# Load the best validated SVM model
+with open('svm_models/svm_0.5.p', 'rb') as f:
+    svm_model = pickle.load(f)
+    
+# Load the tf-idf data
+with open('processed_data/data_tfidf.p', 'rb') as f:
+    train_tfidf = pickle.load(f).toarray()
+    train_dev_tfidf = pickle.load(f).toarray()
+    dev_tfidf = pickle.load(f).toarray()
+    test_tfidf = pickle.load(f).toarray()
+
+# Prepare the classes
+train = pd.read_csv('data/train.csv')
+classes = train['class'].unique()
+encoder = preprocessing.LabelEncoder()
+encoder.fit(classes)
+
+# Load the testing data
+test = pd.read_csv('data/test.csv')
+
+# Prepare the testing data
+test_before = test['before'].tolist()
+test_after = test['after'].tolist()
+test_class = test['class'].tolist()
+
+# Predict
+svm_test = pp.pipeline_predict(token_nb_model, svm_model, nh.normalize_token, 
+                               test_before, train_tfidf, encoder, 
+                               test_after, test_class)
+svm_test_acc = pp.find_accuracy(svm_test)
+
+# Log the process
+with open('final_analysis/test_accuracy.txt', 'w') as f:
+    f.write('SVM model' + '\n')
+    f.write('testing accuracy: ' + str(svm_test_acc) + '\n')
+
+
+
